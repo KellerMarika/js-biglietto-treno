2d9a8ddc9d59403d74a50d7da03561c4fa521902
KellerMarika <marikakeller92@gmail.com>
2022-10-27 15:05:22 +0200

    if fixed

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 /* DATI UTENTE */
 //Km 
 //il parseInt esclude eventuali stringhe aggiuntive e utilizzare il primo valore numenrico come valore di tipo number
-const userTravelWidth = parseInt (prompt("inserire il numero di chilometri da percorrere"));
+const userTravelWidth = parseInt(prompt("inserire il numero di chilometri da percorrere"));
 console.log("n km = ", userTravelWidth);
 //anno nascita
-const userBirthYear= parseInt (prompt("inserire il proprio anno di nascita"));
+const userBirthYear = parseInt(prompt("inserire il proprio anno di nascita"));
 console.log("anno nascita user = ", userBirthYear);
 
 //recupero data corrente:
-const currentData= new Date;
+const currentData = new Date;
 // console.log (currentData); 
 
 //estraggo solamente l'anno dalla data corrente
@@ -20,32 +20,45 @@ const currentYear= (new Date).getFullYear();
 console.log (currentYear); */
 
 //ottengo l'età effettiva dell'utente sottraendo dall'anno corrente il dato inserito dall'utente
-const userAge = currentYear-userBirthYear;
+const userAge = currentYear - userBirthYear;
 console.log(userAge);
 
 
 /* calcolo prezzo senza sconti €/km */
 
-const price = (userTravelWidth * .21)
-console.log(price)
-
-/* const userPrice = `${firstPrice} €`
-console.log(userPrice)
- */
+const price = (userTravelWidth * .21);
+console.log(price);
 
 
-//condizioni
+/* dichiaro priceList ma assegno il valore dentro le if/else */
+let priceList
+
+//condizioni  
 //se utente minorenne : sconto 20%
- if (userAge<18){
-    console.log("user minorenne");
+//calc: x:price=%:100  
 
-}else if (userAge>=65){
-    console.log("user over 65");
-//se utente over65 : sconto 40%    
-}else{
-    console.log("user non ne ha voglia")
+if (userAge < 18) {
+//price - price * 20 /100
+    priceList = (price - price * .2);
+    console.log("priceList miniorenne =", priceList);
+
+    //se utente over65 : sconto 40%  
+} else if (userAge >= 65) {
+
+    priceList = (price - price * .4);
+    console.log("priceList over65 =", priceList);
+
+/* tutti coloro che non sono nè minorenni nè over 65 */
+} else {
+
+    priceList = price
+    console.log("userPrice 65>user>18=", priceList);
 }
- 
 
 //a tutti aggiungo l'euro e tolgo i decimali in eccesso
 //per fatrlo ho bisogno che la voariabile uscente abbia lo stesso nome per tutti gli if
+
+userPrice = `${priceList} €`;
+console.log(userPrice);
+
+
